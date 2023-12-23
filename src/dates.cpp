70b13f3cb4d61561af894d75a7f62a4e569343b2
Joaquin Bejar <jb@taunais.com>
2023-12-23 18:51:28 +0100

    VERSION 0.1.1 add remove_quotes

--- src/dates.cpp
+++ src/dates.cpp
@@ -23,6 +23,20 @@ namespace common::dates {
 
 
 
+    std::string get_current_date() {
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+
+        std::tm now_tm = *std::localtime(&now_time);
+
+        std::stringstream ss;
+        ss << std::put_time(&now_tm, "%Y-%m-%d");
+
+        return ss.str();
+    }
+
+
+
 
 
 }
