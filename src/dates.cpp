90472017f2754343cb7951fb35abede6709f3401
Joaquin Bejar <jb@taunais.com>
2023-12-25 07:19:59 +0100

    VERSION 0.1.2 dates iterator

--- src/dates.cpp
+++ src/dates.cpp
@@ -21,8 +21,6 @@ namespace common::dates {
         return time_t_now;
     }
 
-
-
     std::string get_current_date() {
         auto now = std::chrono::system_clock::now();
         std::time_t now_time = std::chrono::system_clock::to_time_t(now);
@@ -35,8 +33,50 @@ namespace common::dates {
         return ss.str();
     }
 
+    bool isWeekend(const std::chrono::system_clock::time_point &date) {
+        auto timeT = std::chrono::system_clock::to_time_t(date);
+        auto localTime = *std::localtime(&timeT);
+        return localTime.tm_wday == 0 || localTime.tm_wday == 6; // Sunday or Saturday
+    }
+
+    std::string formatDate(const std::chrono::system_clock::time_point &date) {
+        auto timeT = std::chrono::system_clock::to_time_t(date);
+        std::tm tm = *std::localtime(&timeT);
+        std::stringstream ss;
+        ss << std::put_time(&tm, "%Y-%m-%d");
+        return ss.str();
+    }
+
+    DateIterator::DateIterator(int daysToEnd)
+            : date_(std::chrono::system_clock::now()),
+              endDate_(date_ - std::chrono::days(daysToEnd)) {}
+
+    DateIterator::Date DateIterator::operator*() {
+        return formatDate(date_);
+    }
+
+    DateIterator &DateIterator::operator++() {
+        do {
+            date_ -= std::chrono::days(1);
+        } while (isWeekend(date_));
+        return *this;
+    }
+
+    DateIterator DateIterator::operator++(int) {
+        DateIterator temp = *this;
+        ++(*this);
+        return temp;
+    }
 
 
+    DateRange::DateRange(int years) : years_(years) {}
 
+    DateIterator DateRange::begin() {
+        return DateIterator(0);
+    }
+
+    DateIterator DateRange::end()  {
+        return DateIterator(years_ * 365); // Simplification, does not account for leap years
+    }
 
 }
---
+++ test/test_dates.cpp
@@ -0,0 +1,70 @@
+//
+// Created by Joaquin Bejar Garcia on 25/12/23.
+//
+#include <common/common.h>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <common/dates.h>
+#include <set>
+#include <regex>
+
+using common::dates::DateRange;
+
+TEST_CASE("DateRange Print", "[DateRange]") {
+    SECTION("all days") {
+        DateRange dateRange(1); // 1 año
+        for (const auto &dateStr: dateRange) {
+            std::cout << dateStr << std::endl;
+        }
+    }
+}
+
+TEST_CASE("DateRange Tests", "[DateRange]") {
+    SECTION("Format Check") {
+        DateRange dateRange(1); // 1 año
+        for (const auto &dateStr: dateRange) {
+            std::regex dateFormat(R"(\d{4}-\d{2}-\d{2})");
+            REQUIRE(std::regex_match(dateStr, dateFormat));
+        }
+    }
+
+    SECTION("Weekend Exclusion") {
+        DateRange dateRange(0);
+        std::set<int> weekendDays = {0, 6};
+
+        for (const auto& dateStr : dateRange) {
+            std::tm tm = {};
+            std::istringstream ss(dateStr);
+            ss >> std::get_time(&tm, "%Y-%m-%d");
+            std::time_t timeT = std::mktime(&tm);
+
+            REQUIRE(weekendDays.find(tm.tm_wday) == weekendDays.end());
+        }
+    }
+
+    SECTION("Correct Number of Days") {
+        int years = 1;
+        int expectedDays = 261; // 365 - 52*2
+        DateRange dateRange(years);
+
+        int dayCount = 0;
+        for (auto it = dateRange.begin(); it != dateRange.end(); ++it) {
+            dayCount++;
+        }
+
+        REQUIRE(dayCount <= expectedDays);
+    }
+
+    SECTION("Correct Number of Days 5 years ") {
+        int years = 5;
+        int expectedDays = 1305; // (365 - 52*2) * 5
+        DateRange dateRange(years);
+
+        int dayCount = 0;
+        for (auto it = dateRange.begin(); it != dateRange.end(); ++it) {
+            dayCount++;
+        }
+
+        REQUIRE(dayCount < expectedDays);
+    }
+}
