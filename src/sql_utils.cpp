f2e2818ba1d9898a3ac9b3c77d244746afe3dec3
Joaquin Bejar <jb@taunais.com>
2023-12-29 09:17:56 +0100

    VERSION 0.1.3 add QUERYREGEX and InsertType

--- src/sql_utils.cpp
+++ src/sql_utils.cpp
@@ -4,6 +4,7 @@
 
 #include <common/sql_utils.h>
 
+
 namespace common::sql_utils {
     std::string empty_to_null(const std::string &query) {
         std::string modifiedQuery = query;
@@ -29,4 +30,33 @@ namespace common::sql_utils {
         }
         return result;
     }
+
+    void replace_insert_type(std::string &query, const InsertType &insert_type) {
+        // Use regular expressions for case-insensitive search and to handle extra spaces
+        for (const auto &it: insert_type_map) {
+            // Construct a regex pattern to match optional spaces at the beginning and within the string
+            std::string pattern = "\\s*";  // Optionally match leading spaces
+            for (char c: it.second) {
+                if (c == ' ') {
+                    pattern += "\\s*";  // Replace spaces with regex for optional spaces
+                } else {
+                    pattern += c;
+                }
+            }
+
+            std::regex regex_pattern(pattern, std::regex_constants::icase);
+
+            // Search and replace using regex
+            if (std::regex_search(query, regex_pattern)) {
+                const std::string &replacement = insert_type_map.at(insert_type);
+                query = std::regex_replace(query, regex_pattern, replacement, std::regex_constants::format_first_only);
+                break;
+            }
+        }
+    }
+
+    bool is_insert_or_replace_query_correct(const std::string &query) {
+        return std::regex_match(query, QUERYREGEX);
+    }
+
 }
