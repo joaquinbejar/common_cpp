b55d6a5d41030bfeb979aca92662ad754770972e
Joaquin Bejar <jb@taunais.com>
2026-09-19 09:30:14 +0200

    chore: release v0.1.6

--- cmake/simple_color.cmake
+++ cmake/simple_color.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(simple_color
         GIT_REPOSITORY https://github.com/joaquinbejar/simple_color.git
-        GIT_TAG v0.1.0
+        GIT_TAG v0.1.1
 )
 FetchContent_MakeAvailable(simple_color)
 
